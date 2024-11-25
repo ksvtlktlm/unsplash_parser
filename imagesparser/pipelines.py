8476d5faa00a82eb0cac9e211beb015c45fd0451
Diana <diana.bakumenko@gmail.com>
2024-11-25 16:24:12 +0100

    seminar6_1

--- imagesparser/pipelines.py
+++ imagesparser/pipelines.py
@@ -10,6 +10,7 @@ from itemadapter import ItemAdapter
 
 
 import csv
+from scrapy.exceptions import DropItem
 
 class ImagesparserPipeline:
     def process_item(self, item, spider):
@@ -21,11 +22,17 @@ class CsvExportPipeline:
         self.file = open('images_data.csv', 'w', newline='', encoding='utf-8')
         self.writer = csv.writer(self.file, delimiter=';')
         self.writer.writerow(['Title', 'Category', 'Image URL', 'Local Path'])
+        self.seen_urls = set()
 
     def close_spider(self, spider):
         self.file.close()
 
     def process_item(self, item, spider):
+        unique_id = item['url']
+        if unique_id in self.seen_urls:
+            raise DropItem(f"Duplicate item found: {item}")
+        self.seen_urls.add(unique_id)
+
         image_path = item.get('images', [{}])[0].get('path', 'N/A')
         self.writer.writerow([item['title'], item['category'], item['url'], image_path])
 
